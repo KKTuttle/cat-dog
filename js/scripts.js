4bd479cc8f77d436bb10cf2a20e6d6a9169eded1
Adam Luchini and Katarina Karolcikova <katarina@>
2016-05-31 16:20:25 -0700

    working version with colors

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,12 @@ $(document).ready(function() {
   $("button#hello").click(function() {
     $("ul#dog").prepend("<li>arf, hav</li>");
     // $("ul#cat").prepend("<li>meow, mnau</li>");
-    $('li').css('background-color', 'green');
+    $('#dog').css('background-color', 'green');
   });
 
   $("button#goodbye").click(function() {
     // $("ul#dog").prepend("<li>Bark!</li>");
     $("ul#cat").prepend("<li>Hiss!</li>");
-    $('li').css('background-color', 'red');
+    $('#cat').css('background-color', 'red');
   });
 });
